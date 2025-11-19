1f1ef1494b56d009345a5680a469abcc33725da2
paulflagel <paulflagel@gmail.com>
2025-11-19 23:59:43 +0100

    format the system and user prompts to generate the conversations summary

--- chat_analyser/core/analyser.py
+++ chat_analyser/core/analyser.py
@@ -1,14 +1,23 @@
 from mistralai import Mistral
-import json
 from os.path import join as pjoin
 import markdown
 from chat_analyser.api.models import ConversationAnalysisResponse
 from chat_analyser import config as cf
+from typing import Literal
 
-client = Mistral(api_key=cf.API_KEY)
 
+def load_system_prompt(context_type: Literal["party", "work"]) -> str:
+    """Loads the context to be used as a system prompt.
 
-def load_context(context_type: str) -> str:
+    Args:
+        context_type (Literal["party", "work"]): The name of the context to load.
+
+    Raises:
+        ValueError: If the requested context type does not exist.
+
+    Returns:
+        str: The loaded context as a string.
+    """
     if context_type not in cf.AVAILABLE_CONTEXTS:
         raise ValueError(
             f"Context type {context_type} not among existing context. Please choose one among : {cf.AVAILABLE_CONTEXTS}"
@@ -17,40 +26,40 @@ def load_context(context_type: str) -> str:
         return markdown.markdown(f.read())
 
 
-# TODO: Use a json format with {user: message} instead of two lists.
-def format_query(messages: list[dict[str, str]], users: list[str]) -> str:
-    messages = "\n".join([json.dumps(message) for message in messages])
-    users = "\n".join(users)
-    return messages + "\n" + users
+def format_query(conversations: list[dict[str, str]]) -> str:
+    prompt = "Here are the messages sent:\n{json_messages}".format(
+        json_messages=conversations
+    )
+    return prompt
 
 
 def analyse_chat(
     context_type: str,
-    messages: list[dict[str, str]],
-    users: list[str],
+    conversations: list[dict[str, str]],
     model: str = cf.MISTRAL_MODEL,
 ) -> ConversationAnalysisResponse:
-    
-    system_prompt: str = load_context(context_type)
-    user_prompt: str = format_query(context_type, messages, users)
+    system_prompt: str = load_system_prompt(context_type)
+    user_prompt: str = format_query(conversations)
 
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_prompt},
     ]
 
-    chat_response = (
-        client.chat.complete(
-            model=model,
-            messages=messages,
-            response_format={
-                "type": "json_object",
-                "json_schema": ConversationAnalysisResponse.model_json_schema(),
-            },
+    with Mistral(api_key=cf.API_KEY) as client:
+        chat_response = (
+            client.chat.complete(
+                model=model,
+                messages=messages,
+                stream=False,
+                response_format={
+                    "type": "json_object",
+                    "json_schema": ConversationAnalysisResponse.model_json_schema(),
+                },
+            )
+            .choices[0]
+            .message.content
         )
-        .choices[0]
-        .message.content
-    )
     chat_response = chat_response[
         chat_response.index("{") : chat_response.rindex("}") + 1
     ]

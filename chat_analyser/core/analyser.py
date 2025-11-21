c8d80ac75712bf36e1e0b1cfd9551f5acefb56db
paulflagel <paulflagel@gmail.com>
2025-11-22 00:21:43 +0100

    enh(core): Added the users list to the model's user-prompt, and improved docs.

--- chat_analyser/core/analyser.py
+++ chat_analyser/core/analyser.py
@@ -25,20 +25,56 @@ def load_system_prompt(context_type: str) -> str:
         return markdown.markdown(f.read())
 
 
-def format_query(conversations: list[dict[str, str]]) -> str:
-    prompt = "Here are the messages sent:\n{json_messages}".format(
-        json_messages=conversations
+def format_user_prompt(
+    users: list[str], conversations: dict[int, dict[str, str]]
+) -> str:
+    """Format the user prompt given the list of users, and the conversations dictionary.
+
+    Args:
+        users (list[str]): The list of users.
+        conversations (dict[int, dict[str, str]]): The conversations dictionary
+
+    Returns:
+        str: The formatted user prompt.
+    """
+
+    template = (
+        """## Users list\n{users}\n\n"""
+        """## Conversations\n{json_messages}"""
     )
-    return prompt
+
+    formatted_prompt = template.format(users=users, json_messages=conversations)
+    return formatted_prompt
 
 
 def analyse_chat(
     context_type: str,
+    users: list[str],
     conversations: list[dict[str, str]],
     model: str = cf.MISTRAL_MODEL,
 ) -> ConversationAnalysisResponse:
+    """Call a Mistral LLM to analyse the conversations of users given a context type.
+    The model outputs relevant informations defined in the system prompt (which depends on the context type), in a structured format.
+
+    Here is the expected format for the conversations dict:
+    ```
+    {
+        0: {"user": "Alice", "content": "Hi Bob, please find attached the documents for the meeting."},
+        1: {"user": "Bob", "content": "Thank you Alice. I'm looking forward to see your presentation."},
+    }
+    ```
+
+    Args:
+        context_type (str): The name of the context to load.
+        users (list[str]): The list of users registered in the chat.
+        conversations (list[dict[str, str]]): The conversations dictionary.
+        model (str, optional): The name of the Mistral model to call. Defaults to cf.MISTRAL_MODEL.
+
+    Returns:
+        ConversationAnalysisResponse: A pydantic ConversationAnalysisResponse object containing the analysis results.
+    """
     system_prompt: str = load_system_prompt(context_type)
-    user_prompt: str = format_query(conversations)
+    user_prompt: str = format_user_prompt(users, conversations)
 
     messages = [
         {"role": "system", "content": system_prompt},
